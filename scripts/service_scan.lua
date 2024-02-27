dbf758ed513dffb0a4d6604c83b4ad3fdb6ea0e1
vamastah <szymorawski@gmail.com>
2024-02-27 10:47:46 +0100

    add service scanner, tweak session scanner

---
+++ scripts/service_scan.lua
@@ -0,0 +1,108 @@
+-- session scanner for UDS
+
+-- scan settings
+diag_req  = 0x18DA0BFA
+diag_resp = 0x18DAFA0B
+timeout = 2000
+
+session_id = 0x01
+service_id = 0x00
+
+function switch_session(sid)
+	msg = {}
+	msg.id = diag_req
+	msg.eff = true
+	msg.len = 8
+
+	msg[0] = 0x02
+	msg[1] = 0x10
+	msg[2] = sid
+	for i = 3, msg.len-1 do
+		msg[i] = 0xcc
+	end
+
+	emit(msg)
+	set_timer(timeout)
+end
+
+function tester_present()
+	msg = {}
+	msg.id = diag_req
+	msg.eff = true
+	msg.len = 8
+
+	msg[0] = 0x02
+	msg[1] = 0x3e
+	msg[2] = 0x00
+	for i = 3, msg.len-1 do
+		msg[i] = 0xcc
+	end
+
+	emit(msg)
+	set_timer(timeout)
+end
+
+function check_service(sid)
+	msg = {}
+	msg.id = diag_req
+	msg.eff = true
+	msg.len = 8
+
+	msg[0] = 0x01
+	msg[1] = sid
+	for i = 2, msg.len-1 do
+		msg[i] = 0xcc
+	end
+
+	emit(msg)
+	set_timer(timeout)
+end
+
+function scan_next_service_or_die()
+	if service_id < 0xff then
+		service_id = service_id + 1
+		check_service(service_id)
+	else
+		disable_node()
+	end
+end
+
+function on_enable()
+	io.write(string.format("Service scan for session 0x%02x started\n", session_id))
+	switch_session(session_id)
+end
+
+function on_disable()
+	io.write(string.format("Service scan for session 0x%02x stopped\n", session_id))
+	set_timer(0)	-- optional call
+end
+
+function on_timer(ms)
+	io.write(string.format("Service 0x%02x - no response\n", service_id))
+	scan_next_service_or_die()
+end
+
+function on_message(msg)
+	if msg.id == diag_resp then
+		if msg[1] == 0x50 then
+			io.write(string.format("Switched to 0x%02x session\n", session_id))
+			service_id = 0x00
+			check_service(service_id)
+		elseif msg[1] == (service_id + 0x40) or (msg[1] == 0x7f and msg[2] == service_id and msg[3] ~= 0x11 and msg[3] ~= 0x7f) then
+		-- 0x11 - serviceNotSupported
+		-- 0x7f - serviceNotSupportedInActiveSession
+			io.write(string.format("Service 0x%02x present - ", service_id))
+			if msg[1] == (service_id + 0x40) then
+				io.write(string.format("positive response\n"))
+			else
+				io.write(string.format("NRC 0x%02x\n", msg[3]))
+			end
+			scan_next_service_or_die()
+		elseif msg[1] == 0x7f and msg[3] == 0x78 then
+		-- 0x78 - requestCorrectlyReceived-ResponsePending
+			tester_present()
+		else
+			scan_next_service_or_die()
+		end
+	end
+end
--- scripts/session_scan.lua
+++ scripts/session_scan.lua
@@ -3,7 +3,7 @@
 -- scan settings
 diag_req  = 0x18DA0BFA
 diag_resp = 0x18DAFA0B
-timeout = 20
+timeout = 2000
 
 session_id = 0x01
 
@@ -41,6 +41,15 @@ function tester_present()
 	set_timer(timeout)
 end
 
+function scan_next_session_or_die()
+	if session_id < 0xff then
+		session_id = session_id + 1
+		switch_session(session_id)
+	else
+		disable_node()
+	end
+end
+
 function on_enable()
 	print("Session scan started")
 	switch_session(session_id)
@@ -52,25 +61,15 @@ function on_disable()
 end
 
 function on_timer(ms)
-	io.write(string.format("sid %02x - no response\n", session_id))
-	if session_id < 0xff then
-		session_id = session_id + 1
-		switch_session(session_id)
-	else
-		disable_node()
-	end
+	io.write(string.format("sid 0x%02x - no response\n", session_id))
+	scan_next_session_or_die()
 end
 
 function on_message(msg)
 	if msg.id == diag_resp then
 		if msg[1] == 0x50 then
-			io.write(string.format("sid %02x present - positive response\n", session_id))
-			if session_id < 0xff then
-				session_id = session_id + 1
-				switch_session(session_id)
-			else
-				disable_node()
-			end
+			io.write(string.format("sid 0x%02x present - positive response\n", session_id))
+			scan_next_session_or_die()
 		elseif msg[1] == 0x7f and msg[2] == 0x10 then
 			if msg[3] == 0x78 then -- requestCorrectlyReceived-ResponsePending
 				tester_present();
@@ -80,20 +79,13 @@ function on_message(msg)
 			-- 0x33 - securityAccessDenied
 			-- 0x34 - authenticationRequired
 			if msg[3] == 0x7e or msg[3] == 0x33 or msg[3] == 0x34 then
-				io.write(string.format("sid %02x present - NRC %02x\n", session_id, msg[3]))
-				if session_id < 0xff then
-					session_id = session_id + 1
-					switch_session(session_id)
-				else
-					disable_node()
-				end
+				io.write(string.format("sid 0x%02x present - NRC 0x%02x\n", session_id, msg[3]))
+				scan_next_session_or_die()
 			elseif msg[3] ~= 0x78 then
-				if session_id < 0xff then
-					session_id = session_id + 1
-					switch_session(session_id)
-				else
-					disable_node()
+				if msg[3] ~= 0x31 then	-- requestOutOfRange
+					io.write(string.format("sid 0x%02x ??????? - NRC 0x%02x\n", session_id, msg[3]))
 				end
+				scan_next_session_or_die()
 			end
 		end
 	end
